4865359fc313881a083a915f52c9afc2209901e9
Tiger Zhao <980401@gmail.com>
2020-08-03 14:34:47 -0700

    Fully implemented save and load methods. Added gson.jar to .lib

--- src/main/persistence/Saveable.java
+++
@@ -1,9 +0,0 @@
-package persistence;
-import java.io.FileWriter;
-
-// Represents data that can be saved to file
-public interface Saveable {
-    // MODIFIES: printWriter
-    // EFFECTS: writes the saveable to printWriter
-    void save(FileWriter printWriter);
-}
